9fb5a591a513e9c5e4699d9d08da9ed064f1071d
M.Ali Yücesoylu <myucesoylu@gmail.com>
2022-12-30 14:25:21 +0300

    initial layout created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "pages/homePage";
+import LoginPage from "pages/loginPage";
+import ProfilePage from "pages/profilePage";
 
 function App() {
   return (
     <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
