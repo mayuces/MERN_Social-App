7d5e757574388c592aa3e1ea532725667f2422ee
M.Ali Yücesoylu <myucesoylu@gmail.com>
2023-01-02 12:41:31 +0300

    color, theme, and styling created

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/system";
+import { styled } from "@mui/material/styles";
+
+const FlexBetween = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+export default FlexBetween;
