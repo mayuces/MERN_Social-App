0f0ec730f1183ae0f1131ab0c5e4889b7f1fda56
M.Ali Yücesoylu <myucesoylu@gmail.com>
2023-01-03 13:03:47 +0300

    post component and functionality created

--- client/src/pages/homePage/index.jsx
+++ client/src/pages/homePage/index.jsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "pages/navbar";
 import UserWidget from "pages/widgets/UserWidget";
+import MyPostWidget from "pages/widgets/MyPostWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -24,7 +25,7 @@ const HomePage = () => {
           flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
-          Main Content
+          <MyPostWidget picturePath={picturePath}/>
         </Box>
       </Box>
     </Box>
